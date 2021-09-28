d27f292e73437b9c897574b681d18402ddb5e82a
Onaconnapuna <cjgermain12@gmail.com>
2021-09-27 20:38:56 -0400

    working on sessionReducer

---
+++ frontend/reducers/root_reducer.js
@@ -0,0 +1,9 @@
+import { combineReducers } from "redux";
+import entitiesReducer from "./entities_reducer";
+
+
+const RootReducer = combineReducers({
+  entities: entitiesReducer
+});
+
+export default RootReducer
---
+++ frontend/reducers/session_reducer.js
@@ -0,0 +1,7 @@
+import RECEIVE_CURRENT_USER from '../actions/session_actions';
+
+const _nullUser = Object.freeze({
+  id: null
+});
+
+// const sessionReducer - (s)
