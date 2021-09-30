1682162dc515874fec34b84ab62f185166dbb79a
Onaconnapuna <cjgermain12@gmail.com>
2021-09-30 15:11:24 -0400

    using old store

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -5,18 +5,18 @@ import logger from 'redux-logger'
 
 let configureStore;
 
-if (process.env.NODE_ENV === 'production') {
-  configureStore = (preloadedState = {}) => {
-    return createStore(RootReducer, preloadedState, applyMiddleware(thunk))
-  };
-} else {
-  configureStore = (preloadedState = {}) => {
-    return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-  };
-}
+// if (process.env.NODE_ENV === 'production') {
+//   configureStore = (preloadedState = {}) => {
+//     return createStore(RootReducer, preloadedState, applyMiddleware(thunk))
+//   };
+// } else {
+//   configureStore = (preloadedState = {}) => {
+//     return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+//   };
+// }
 
-// const configureStore = (preloadedState = {}) => {
-//   return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-// };
+const configureStore = (preloadedState = {}) => {
+  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+};
 
 export default configureStore
