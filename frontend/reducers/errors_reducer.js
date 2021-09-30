c1a8a5a3d1f173f659c8870ea606017a7453b564
Onaconnapuna <cjgermain12@gmail.com>
2021-09-30 14:35:40 -0400

    heroku crashing, trying to find error

--- frontend/reducers/errors_reducer.js
+++ frontend/reducers/errors_reducer.js
@@ -1,6 +1,5 @@
 import { combineReducers } from "redux";
 import SessionErrorsReducer from "./session_errors_reducer";
-import { REMOVE_SESSION_ERRORS } from "../actions/session_actions";
 
 const errorsReducer = combineReducers({
   session: SessionErrorsReducer
