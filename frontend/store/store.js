7e50fb55ca6b4500fad35976812cacd9a2e34a4a
Onaconnapuna <cjgermain12@gmail.com>
2021-09-30 15:35:48 -0400

    or the configureStore

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -3,20 +3,20 @@ import thunk from 'redux-thunk'
 import RootReducer from '../reducers/root_reducer'
 import logger from 'redux-logger'
 
-// let configureStore;
+let configureStore;
 
-// if (process.env.NODE_ENV === 'production') {
-//   configureStore = (preloadedState = {}) => {
-//     return createStore(RootReducer, preloadedState, applyMiddleware(thunk))
-//   };
-// } else {
-//   configureStore = (preloadedState = {}) => {
-//     return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-//   };
-// }
+if (process.env.NODE_ENV === 'production') {
+  configureStore = (preloadedState = {}) => {
+    return createStore(RootReducer, preloadedState, applyMiddleware(thunk))
+  };
+} else {
+  configureStore = (preloadedState = {}) => {
+    return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+  };
+}
 
-const configureStore = (preloadedState = {}) => {
-  return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
-};
+// const configureStore = (preloadedState = {}) => {
+//   return createStore(RootReducer, preloadedState, applyMiddleware(thunk, logger))
+// };
 
 export default configureStore
