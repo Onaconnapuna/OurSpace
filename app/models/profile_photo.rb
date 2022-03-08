e850e6fabebe1b2d3f5bd676fede1a168a85d046
Onaconnapuna <cjgermain12@gmail.com>
2022-03-08 01:53:07 -0500

    quick adj

--- app/models/profile_photo.rb
+++ app/models/profile_photo.rb
@@ -9,12 +9,12 @@ class ProfilePhoto < ApplicationRecord
 
   has_one_attached :photo
 
-  # after_save :attach_default_photo
+  after_save :attach_default_photo
 
-  # def attach_default_photo
-  #   if !self.photo.attached?
-  #     file = URI.open('https://ourspace-fullstackproject-dev.s3.us-east-2.amazonaws.com/default_profile_pic.jpg')
-  #     self.photo.attach(io: file, filename: 'default_profile_pic.png')
-  #   end
-  # end
+  def attach_default_photo
+    if !self.photo.attached?
+      file = URI.open('https://ourspace-fullstackproject-dev.s3.us-east-2.amazonaws.com/default_profile_pic.jpg')
+      self.photo.attach(io: file, filename: 'default_profile_pic.png')
+    end
+  end
 end
